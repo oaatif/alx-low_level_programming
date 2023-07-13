d7ed996b2bf8c2d8b18cb97048dada8f69774161
Aatif <oaatif7@gmail.com>
2023-07-13 12:27:50 -0700

    prg-c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * main_entry point
- * description c programme
- * return always 0
+ * main - Entry point
+ * return:  always 0 (success)
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
