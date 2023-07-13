c40fa165413ab418119f1c1ee0893d1bf16efc67
Aatif <oaatif7@gmail.com>
2023-07-13 06:32:38 -0700

    c5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main_entry point
+ * description c programme
+ * return always 0
+ */
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
