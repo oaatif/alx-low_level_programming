ca2709994f794ce3e7b0421c4ce768ab51ca43b6
Aatif <oaatif7@gmail.com>
2023-07-18 09:55:28 -0700

    puchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+/**
+ * main - Entry point
+ * Description: 'putchar'
+ * retutn always 0(success) 
+ */
+int main(void)
+{
+	_putchar('_')
+	_putchar('p')
+	_putchar('u')
+	_putchar('t')
+	_putchar('c')
+	_putchar('h')
+	_putchar('a')
+	_putchar('r')
+	_putchar('\n')
+	retutn (0);
+
+
+
